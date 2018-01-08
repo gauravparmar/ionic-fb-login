c3f2e66bba7e0b9d5892877cc0950e89dd56ab35
gauravparmar <gauravparmariam@gmail.com>
2018-01-08 15:15:28 +0530

    Ionic App with facebook login

---
+++ src/pages/home/home.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { Facebook } from '@ionic-native/facebook';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  isLoggedIn:boolean = false;
+  users: any;
+
+  constructor(private fb: Facebook) {
+    fb.getLoginStatus()
+      .then(res => {
+        console.log(res.status);
+        if(res.status === "connect") {
+          this.isLoggedIn = true;
+        } else {
+          this.isLoggedIn = false;
+        }
+      })
+      .catch(e => console.log(e));
+  }
+
+  login() {
+    console.log('Login clicked');
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+      .then(res => {
+        console.log('Got responose');
+        if(res.status === "connected") {
+          this.isLoggedIn = true;
+          console.log(res);
+          this.getUserDetail(res.authResponse.userID);
+        } else {
+          this.isLoggedIn = false;
+        }
+      })
+      .catch(e => console.log('Error logging into Facebook', e));
+  }
+
+  logout() {
+    this.fb.logout()
+      .then( res => this.isLoggedIn = false)
+      .catch(e => console.log('Error logout from Facebook', e));
+  }
+
+  getUserDetail(userid) {
+    this.fb.api("/"+userid+"/?fields=id,email,name,picture,gender",["public_profile"])
+      .then(res => {
+        console.log(res);
+        this.users = res;
+        
+        console.log("User data "+this.users);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  }
+
+}
